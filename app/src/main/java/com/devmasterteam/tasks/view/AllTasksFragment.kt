65f6ad13c58e18f171c602730cf3937100796b1d
Sérgio <sergioriosribeiro@outlook.com>
2024-02-16 19:03:47 -0300

    Configuração da drawer e logout

--- app/src/main/java/com/devmasterteam/tasks/view/AllTasksFragment.kt
+++ app/src/main/java/com/devmasterteam/tasks/view/AllTasksFragment.kt
@@ -1,5 +1,6 @@
 package com.devmasterteam.tasks.view
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -11,6 +12,7 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.devmasterteam.tasks.databinding.FragmentAllTasksBinding
+import com.devmasterteam.tasks.service.constants.TaskConstants
 import com.devmasterteam.tasks.service.listener.TaskListener
 import com.devmasterteam.tasks.view.adapter.TaskAdapter
 import com.devmasterteam.tasks.viewmodel.TaskListViewModel
@@ -22,6 +24,7 @@ class AllTasksFragment : Fragment() {
     private val binding get() = _binding!!
 
     private val adapter = TaskAdapter()
+    private var taskFilter = 0
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, b: Bundle?): View {
         viewModel = ViewModelProvider(this).get(TaskListViewModel::class.java)
@@ -30,8 +33,15 @@ class AllTasksFragment : Fragment() {
         binding.recyclerAllTasks.layoutManager = LinearLayoutManager(context)
         binding.recyclerAllTasks.adapter = adapter
 
+        taskFilter = requireArguments().getInt(TaskConstants.BUNDLE.TASKFILTER, 0)
+
         val listener = object : TaskListener {
             override fun onListClick(id: Int) {
+                val intent = Intent(context, TaskFormActivity::class.java)
+                val bundle = Bundle()
+                bundle.putInt(TaskConstants.BUNDLE.TASKID, id)
+                intent.putExtras(bundle)
+                startActivity(intent)
             }
 
             override fun onDeleteClick(id: Int) {
@@ -39,15 +49,17 @@ class AllTasksFragment : Fragment() {
             }
 
             override fun onCompleteClick(id: Int) {
+                viewModel.status(id, true)
             }
 
             override fun onUndoClick(id: Int) {
+                viewModel.status(id, false)
             }
         }
 
         adapter.attachListener(listener)
 
-        viewModel.list()
+
 
         // Cria os observadores
         observe()
@@ -57,7 +69,7 @@ class AllTasksFragment : Fragment() {
 
     override fun onResume() {
         super.onResume()
-        viewModel.list()
+        viewModel.list(taskFilter)
     }
 
     override fun onDestroyView() {
@@ -70,6 +82,11 @@ class AllTasksFragment : Fragment() {
             adapter.updateTasks(it)
         }
         viewModel.delete.observe(viewLifecycleOwner) {
+            if (!it.status()) {
+                Toast.makeText(context, it.message(), Toast.LENGTH_SHORT).show()
+            }
+        }
+        viewModel.status.observe(viewLifecycleOwner) {
             if (!it.status()) {
                 Toast.makeText(context, it.message(), Toast.LENGTH_SHORT).show()
             }
--- app/src/main/java/com/devmasterteam/tasks/view/TaskFormActivity.kt
+++ app/src/main/java/com/devmasterteam/tasks/view/TaskFormActivity.kt
@@ -10,6 +10,7 @@ import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import com.devmasterteam.tasks.R
 import com.devmasterteam.tasks.databinding.ActivityTaskFormBinding
+import com.devmasterteam.tasks.service.constants.TaskConstants
 import com.devmasterteam.tasks.service.model.PriorityModel
 import com.devmasterteam.tasks.service.model.TaskModel
 import com.devmasterteam.tasks.viewmodel.TaskFormViewModel
@@ -23,6 +24,7 @@ class TaskFormActivity : AppCompatActivity(), View.OnClickListener,
     private lateinit var binding: ActivityTaskFormBinding
     private val dateFormat: SimpleDateFormat = SimpleDateFormat("dd/MM/yyyy")
     private var listPriority: List<PriorityModel> = mutableListOf()
+    private var taskIdentification = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,6 +38,8 @@ class TaskFormActivity : AppCompatActivity(), View.OnClickListener,
         binding.buttonDate.setOnClickListener(this)
 
         viewModel.loadPriorities()
+
+        loadDataFromActivity()
 
         observe()
 
@@ -59,6 +63,25 @@ class TaskFormActivity : AppCompatActivity(), View.OnClickListener,
         binding.buttonDate.text = dueDate
     }
 
+    private fun loadDataFromActivity() {
+        val bundle = intent.extras
+        if (bundle != null) {
+            taskIdentification = bundle.getInt(TaskConstants.BUNDLE.TASKID)
+            viewModel.load(taskIdentification)
+        }
+    }
+
+    private fun getIndex(priorityId: Int): Int {
+        var index = 0
+        for (l in listPriority) {
+            if (l.id == priorityId) {
+                break
+            }
+            index++
+        }
+        return index
+    }
+
     private fun observe() {
         viewModel.priorityList.observe(this) {
             listPriority = it
@@ -72,10 +95,30 @@ class TaskFormActivity : AppCompatActivity(), View.OnClickListener,
 
         viewModel.taskSave.observe(this) {
             if (it.status()) {
-                toast(applicationContext.getString(R.string.sucesso))
+
+                if (taskIdentification == 0) {
+                    toast(applicationContext.getString(R.string.task_created))
+                } else {
+                    toast(applicationContext.getString(R.string.task_updated))
+                }
                 finish()
             } else {
                 toast(it.message())
+            }
+        }
+
+        viewModel.task.observe(this) {
+            binding.editDescription.setText(it.description)
+            binding.checkComplete.isChecked = it.complete
+            binding.spinnerPriority.setSelection(getIndex(it.priorityId))
+            val date = SimpleDateFormat("yyyy-MM-dd").parse(it.dueDate)
+            binding.buttonDate.text = SimpleDateFormat("dd/MM/yyyy").format(date)
+        }
+
+        viewModel.taskLoad.observe(this) {
+            if (!it.status()) {
+                toast(it.message())
+                finish()
             }
         }
     }
@@ -86,7 +129,7 @@ class TaskFormActivity : AppCompatActivity(), View.OnClickListener,
 
     private fun handleSave() {
         val task = TaskModel().apply {
-            this.id = 0
+            this.id = taskIdentification
             val index = binding.spinnerPriority.selectedItemPosition
             this.priorityId = listPriority[index].id
             this.description = binding.editDescription.text.toString()
